1d74906dcb6760d97a1003823699134083d8ec16
Rudy Flores <38470001+rudypflores@users.noreply.github.com>
2019-02-14 10:51:01 -0600

    Creation of BusinessList.js

--- src/components/BusinessList/BusinessList.js
+++ src/components/BusinessList/BusinessList.js
@@ -3,3 +3,28 @@
 	@Author Rudy Flores
 	@Date February 14, 2019
 */
+
+//Import React Model and Style Properties
+import React from 'react';
+import Style from './BusinessList.css';
+
+//Import for simulation of different Businesses
+import Business from '../Business/Business';
+
+//The Business List Component
+class BusinessList extends React.Component {
+	render() {
+		return (
+			<div className="BusinessList">
+				<Business />
+			  	<Business /> 
+			  	<Business /> 
+			  	<Business /> 
+			  	<Business /> 
+			  	<Business />
+			</div>
+		);
+	}
+};
+
+export default BusinessList;
