e9d5dde5b26727f987e793513ded27374751469a
Rudy Flores <38470001+rudypflores@users.noreply.github.com>
2019-02-15 12:15:02 -0600

    Implementation of Props for easier readability of code

--- src/components/Business/Business.js
+++ src/components/Business/Business.js
@@ -10,18 +10,6 @@ import React from 'react';
 //Import of Business.css
 import './Business.css';
 
-//The Business Object
-const business = {imageSrc: 'https://s3.amazonaws.com/codecademy-content/programs/react/ravenous/pizza.jpg',
-	name: 'MarginOtto Pizzeria',
-	address: '1010 Paddington Way',
-	city: 'Flavortown',
-	state: 'NY',
-	zipCode: '10101',
-	category: 'Italian',
-	rating: 4.5,
-	reviewCount: 90
-};
-
 //Main Component Settings
 class Business extends React.Component {
 	render() {
@@ -29,20 +17,20 @@ class Business extends React.Component {
 			<div className="Business">
 			  	<div className="image-container">
 			    	<img 
-			    		src= {business.imageSrc} 
+			    		src= {this.props.business.imageSrc} 
 			    		alt='Full sized margherita pizza'/>
 			  	</div>
-			  		<h2>{business.name}</h2>
+			  		<h2>{this.props.business.name}</h2>
 			 	 <div className="Business-information">
 			    	<div className="Business-address">
-			      		<p>{business.address}</p>
-			      		<p>{business.city}</p>
-			      		<p>{business.state + ' ' + business.zipCode}</p>
+			      		<p>{this.props.business.address}</p>
+			      		<p>{this.props.business.city}</p>
+			      		<p>{this.props.business.state + ' ' + this.props.business.zipCode}</p>
 			    	</div>
 			    <div className="Business-reviews">
-			      	<h3>{business.category}</h3>
-			      	<h3 className="rating">{business.rating}</h3>
-			      	<p>{business.reviewCount}</p>
+			      	<h3>{this.props.business.category}</h3>
+			      	<h3 className="rating">{this.props.business.rating}</h3>
+			      	<p>{this.props.business.reviewCount}</p>
 			    </div>
 			  </div>
 			</div>
